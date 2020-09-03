71e1308589e8b20abcd76ce60affe097dd825cce
Victor Presumido <victor.presumido@gmail.com>
2020-09-03 16:45:43 +0200

    Create loader fn to load data from json

---
+++ src/utils/loader.js
@@ -0,0 +1,7 @@
+import {sampleSize} from 'lodash';
+
+export async function load(count = 10) {
+  const req = await fetch('js/english_10k.json')
+  const res = await req.json()
+  return sampleSize(res, count)
+};
