7ff17a629a748c90e24f53fa209c41ee69a94a1e
Victor Presumido <victor.presumido@gmail.com>
2020-09-04 11:49:16 +0200

    Updates

---
+++ precache-manifest.7b02dcbe48e20cb8dccb0ed36db11ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e6f61ff85ed94e91469f913cddd1e0",
+    "url": "/suitype/index.html"
+  },
+  {
+    "revision": "2b2487450e16460c6f47",
+    "url": "/suitype/static/css/main.724f1ce9.chunk.css"
+  },
+  {
+    "revision": "1cf6f8891e68f0463ae6",
+    "url": "/suitype/static/js/2.7cc14f7e.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/suitype/static/js/2.7cc14f7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b2487450e16460c6f47",
+    "url": "/suitype/static/js/main.522bcd89.chunk.js"
+  },
+  {
+    "revision": "f0704f0e46583a39fbd3",
+    "url": "/suitype/static/js/runtime-main.dfc72d1a.js"
+  }
+]);
--- src/utils/loader.js
+++
@@ -1,7 +0,0 @@
-import {sampleSize} from 'lodash';
-
-export default async function load(count = 10) {
-  const req = await fetch('js/english_10k.json')
-  const res = await req.json()
-  return sampleSize(res, count)
-};
