7dc8df7fb30cdcbe5f0a08a39c755be54cdae176
Victor Presumido <victor.presumido@gmail.com>
2020-09-04 12:26:06 +0200

    Change loader path

--- src/utils/loader.js
+++ src/utils/loader.js
@@ -1,7 +1,7 @@
 import {sampleSize} from 'lodash';
 
 export default async function load(count = 10) {
-  const req = await fetch('js/english_10k.json')
+  const req = await fetch('suitype/js/english_10k.json')
   const res = await req.json()
   return sampleSize(res, count)
 };
